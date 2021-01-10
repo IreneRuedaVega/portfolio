5fec55ae48caee4fea695292e376928a87d67567
IreneRuedaVega <ruedavega.irene@gmail.com>
2021-01-10 18:55:29 +0100

    Add component Navbar component completed

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navbar.scss";
+
+const Navbar = ({ isScrolling }) => {
+  const tothetop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  return (
+    <nav className={`navbar ${isScrolling > 20 ? "scrolling" : null}`}>
+      <div className="navbar__logo" onClick={tothetop}>
+        &#60;Irene
+        <span className="navbar__logo--span">Rueda</span>
+        &#47;&#62;
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
