19ad00d1c082c9d7b6056012e4371a3921db562e
IreneRuedaVega <ruedavega.irene@gmail.com>
2021-01-10 16:13:11 +0100

    Create and start coding the Cover component with his scss

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import Cover from "./components/cover/Cover";
+
+function App() {
+  return (
+    <div className="App">
+      <Cover />
+    </div>
+  );
+}
+
+export default App;
