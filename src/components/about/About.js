8f08f3fa9c2f10750843050f1ad153f3e3cd66eb
IreneRuedaVega <ruedavega.irene@gmail.com>
2021-01-10 19:31:32 +0100

    Add about component unfinished

---
+++ src/components/about/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.scss";
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-container__desc"></div>
+    </div>
+  );
+};
+
+export default About;
