0378f9f42a18c7a6cbe929658b88e91c3972dfed
IreneRuedaVega <ruedavega.irene@gmail.com>
2021-02-02 21:45:04 +0100

    Add loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
+import BeatLoader from "react-spinners/BeatLoader";
 import "../stylesheets/layout/App.scss";
 import Cover from "./cover/Cover";
 import Navbar from "./navbar/Navbar";
@@ -17,20 +18,35 @@ function App() {
     setScrollHeight(position);
   };
 
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 0);
+  }, []);
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
   }, [scrollHeight]);
 
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar isScrolling={scrollHeight} />
-        <Cover />
-        <About />
-        <Projects data={PROJECTS_DATA} />
-        <Skills />
-        <Contact />
-      </BrowserRouter>
+    <div className="App">
+      {loading ? (
+        <div className="loading">
+          <BeatLoader color={"#8787ff"} loading={loading} size={30} />
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Navbar isScrolling={scrollHeight} />
+          <Cover />
+          <About />
+          <Projects data={PROJECTS_DATA} />
+          <Skills />
+          <Contact />
+        </BrowserRouter>
+      )}
     </div>
   );
 }
