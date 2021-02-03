72dbff734ddc133558706a499ba761150e278313
IreneRuedaVega <ruedavega.irene@gmail.com>
2021-02-03 12:40:28 +0100

    Uninstall skeleton loading and delete loading

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,6 @@ import Projects from "./projects/Projects";
 import Skills from "./skills/Skills";
 import Contact from "./contact/Contact";
 import PROJECTS_DATA from "../data/projects";
-import SkeletonCover from "./cover/SkeletonCover";
 
 function App() {
   const [scrollHeight, setScrollHeight] = useState(0);
@@ -22,29 +21,16 @@ function App() {
     window.addEventListener("scroll", handleScroll);
   }, [scrollHeight]);
 
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const timing = setTimeout(() => {
-      setLoading(false);
-    }, 4000);
-    return () => clearTimeout(timing);
-  }, []);
-
   return (
     <div>
-      {loading && <SkeletonCover />}
-      {!loading && (
-        <BrowserRouter>
-          <Navbar isScrolling={scrollHeight} />
-          <Cover />
-          <About />
-          <Projects data={PROJECTS_DATA} />
-          <Skills />
-          <Contact />
-        </BrowserRouter>
-      )}
+      <BrowserRouter>
+        <Navbar isScrolling={scrollHeight} />
+        <Cover />
+        <About />
+        <Projects data={PROJECTS_DATA} />
+        <Skills />
+        <Contact />
+      </BrowserRouter>
     </div>
   );
 }
